00a0ff1b68c4cbcb23bdceef52dfc8d7c4d7f994
NatanDuarte <natanprocopio@gmail.com>
2022-01-22 14:30:10 -0300

    refactoring: putting parsed args into semantic variables

--- __main__.py
+++ __main__.py
@@ -5,12 +5,17 @@ import cv2 as cv
 if __name__ == '__main__':
     args = get_args_from_parser()
 
-    mirror = Mirror(args['input'])
-    result_image = mirror.process(args['reverse'])
+    input_file_path = args['input']
+    mirror = Mirror(input_file_path)
 
-    if args['preview']:
+    use_reverse = args['reverse']
+    result_image = mirror.process(use_reverse)
+
+    is_preview = args['preview']
+    if is_preview:
         mirror.preview(result_image)
 
-    mirror.save(args['output'], result_image)
+    output_filename = args['output']
+    mirror.save(output_filename, result_image)
 
     cv.destroyAllWindows()
